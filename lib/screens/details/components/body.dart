264652729522ad2e69c267343f0739063489df22
johnrm9 <johnrm9@gmail.com>
2020-12-29 01:36:57 -0800

    Partial added Cast & Crew section on details page.

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -18,15 +18,18 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        BackdropAndRating(size: size, movie: movie),
-        const SizedBox(height: kDefaultPadding / 2),
-        TitleDurationAndFavBtn(movie: movie),
-        Genres(movie: movie),
-        PlotSummary(movie: movie),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          BackdropAndRating(size: size, movie: movie),
+          const SizedBox(height: kDefaultPadding / 2),
+          TitleDurationAndFavBtn(movie: movie),
+          Genres(movie: movie),
+          PlotSummary(movie: movie),
+          CastAndCrew(casts: movie.cast),
+        ],
+      ),
     );
   }
 }
@@ -34,13 +37,63 @@ class Body extends StatelessWidget {
 class CastAndCrew extends StatelessWidget {
   const CastAndCrew({
     Key key,
-    @required this.cast,
+    @required this.casts,
   }) : super(key: key);
 
-  final List<Map<String, String>> cast;
+  final List<Map<String, String>> casts;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+        padding: const EdgeInsets.all(kDefaultPadding),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Cast & Crew',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            const SizedBox(height: kDefaultPadding / 2),
+            SizedBox(
+              height: 160,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: casts.length,
+                itemBuilder: (BuildContext context, int index) =>
+                    CastCard(cast: casts[index]),
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class CastCard extends StatelessWidget {
+  const CastCard({
+    Key key,
+    @required this.cast,
+  }) : super(key: key);
+
+  final Map<String, String> cast;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: kDefaultPadding),
+      width: 80,
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 80,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(cast['image']),
+              ),
+            ),
+          ),
+          const SizedBox(height: kDefaultPadding / 2),
+        ],
+      ),
+    );
   }
 }
